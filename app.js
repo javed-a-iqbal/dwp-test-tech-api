b8b869b17e989a7b9ea2a7856b987a55b3ef712c
javed Iqbal <javedzabi@hotmail.com>
2021-09-15 21:42:32 +0100

    [DWP-2]  add more unit tests - code refactoring

--- app.js
+++ app.js
@@ -12,16 +12,15 @@ const errorLog = require('./logger/logger').errorLog;
 app.use(helmet());
 
 
-const server = app.listen(process.env.PORT || 3000, process.env.IP, function (error) {
+app.listen(process.env.PORT || 3000, process.env.IP, function (error) {
     if (error) {
       errorLog.error("Unable to listen for connections ", error);
       process.exit(10);
     } else {
-      const host = server.address().address;
-      const port = server.address().port;
-      successLog.info(`DWP Tech test  App listening at http://${host}:${port}`, { host: host, port: port });
+      
+      successLog.info(`DWP Tech test  App listening at process.env.PORT`, { host: 'localhost', port: process.env.PORT });
      
     }
   });
 
-module.exports = app;
+  module.exports.app = app;
--- test/controllers/usersController.spec.js
+++ test/controllers/usersController.spec.js
@@ -1,21 +1,38 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../../app");
-const expect = chai.expect;
-const nock = require('nock');
-
-const responseData = require('../stubs/data');
+const fs = require('fs');
 const controller = require('../../controllers/usersController');
 const constants = require('../../controllers/utils/constants');
+const { app } = require('../../app');
 
 chai.use(chaiHttp);
 
 describe('DWP-TEST-TECH-API usersController', () => {
   describe("usersController.getUsers", () => {
-    it("should return list of users", async () => {
-    });
+    it('Should respond with list of users with in 50 miles of london or in london', () => {
+     
+      const expected = JSON.parse(fs.readFileSync(`${__dirname}/../stubs/listUsers.json`));
+         chai.request(app)
+        .get('/users/people-living-in-london-or-within-50-miles')
+        .end((err, res) => {
+          // Assertion
+          chai.expect(res).to.have.status(200);
+          chai.expect(res).to.eql(expected);
+          done();
+        });
+    })
 
 
   });
 
 })
+
+
+
+
+// chai.use(chaiHttp);
+// describe('Index', () => {
+//   describe('GET users/london', () => {
+//    ;
+//   });
+// });
